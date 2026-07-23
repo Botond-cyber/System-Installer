c21e1209dee290c2de02471f2b89cd962f9a6b61
Botond-cyber <davidbotond0807@gmail.com>
2026-07-23 22:04:56 +0200

    Enhance SettingsScreen functionality with save and cancel actions, and improve UI refresh on reload

--- lib/ui/screens/settings_screen.py
+++ lib/ui/screens/settings_screen.py
@@ -1,3 +1,5 @@
+import copy
+
 from textual import on
 from textual.app import ComposeResult
 from textual.containers import Horizontal
@@ -11,6 +13,13 @@ from lib.core.settings import Settings
 
 class SettingsScreen(Screen[None]):
     CSS_PATH = resource_path("ui/assets/settings.tcss")
+    BINDINGS = [
+        ("escape", "switch_screen", "Back to main menu"),
+        ("d", "next", "Next"),
+        ("a", "back", "Back"),
+        ("c", "cancel", "cancel"),
+        ("s", "save", "save"),
+    ]
 
     @property
     def ctx(self) -> Context:
@@ -80,9 +89,18 @@ class SettingsScreen(Screen[None]):
                 )
         yield Footer()
 
+    # Set title on mount
     def on_mount(self) -> None:
+        self.logger.log_to_file("mount")
         self.title = "Settings"
 
+    # Ensure UI refresh on reload
+    def on_screen_resume(self) -> None:
+        self.logger.log_to_file("mount")
+        self.original_settings = copy.deepcopy(self.ctx.settings)
+        self.refresh(recompose=True)
+
+    # Switch event listeners
     @on(Switch.Changed)
     def on_switch_changed(self, event: Switch.Changed) -> None:
         if event.switch.id == "enable-dry-run":
@@ -94,6 +112,7 @@ class SettingsScreen(Screen[None]):
         elif event.switch.id == "use-central-repo":
             self.settings.use_central_repo = event.value
 
+    # Button click handlers
     async def on_button_pressed(self, event: Button.Pressed) -> None:
         match event.button.id:
             case "delete-logs-btn":
@@ -105,12 +124,59 @@ class SettingsScreen(Screen[None]):
                 self.notify("Deleted .system-installer file")
 
             case "apply-btn":
-                try:
-                    Settings.save_settings_to_file(self.ctx, self.logger)
-                except:
-                    self.notify("Failed to save settings see logs for more details!")
-                else:
-                    self.notify("Successfully saved the settings!")
+                self.save_settings()
+
+            case "cancel-btn":
+                self.cancel_changes()
 
             case _:
                 pass
+
+    # Save function
+    def save_settings(self):
+        try:
+            Settings.save_settings_to_file(self.ctx, self.logger)
+        except:
+            self.notify("Failed to save settings see logs for more details!")
+        else:
+            self.notify("Successfully saved the settings!")
+            self.app.switch_screen("intro_screen")  # type: ignore
+
+    # Discard function
+    def cancel_changes(self):
+        self.ctx.settings = self.original_settings
+        self.app.switch_screen("intro_screen")  # type: ignore
+
+    # Key bind actions
+    def action_next(self):
+        tabbed_content = self.query_one(TabbedContent)
+        match tabbed_content.active[-1]:
+            case "1":
+                tabbed_content.active = "tab-2"
+            case "2":
+                tabbed_content.active = "tab-3"
+            case "3":
+                tabbed_content.active = "tab-1"
+            case _:
+                pass
+
+    def action_back(self):
+        tabbed_content = self.query_one(TabbedContent)
+        match tabbed_content.active[-1]:
+            case "1":
+                tabbed_content.active = "tab-3"
+            case "2":
+                tabbed_content.active = "tab-1"
+            case "3":
+                tabbed_content.active = "tab-2"
+            case _:
+                pass
+
+    def action_cancel(self):
+        self.cancel_changes()
+
+    def action_switch_screen(self):
+        self.app.switch_screen("intro_screen")  # type: ignore
+
+    def action_save(self):
+        self.save_settings()
